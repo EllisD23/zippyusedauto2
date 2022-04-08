b5d1c3a6551341fc9cd665582ed466463aa88f3e
EllisD23 <43530364+EllisD23@users.noreply.github.com>
2022-04-07 20:54:44 -0500

    Delete zippyusedauto directory

--- zippyusedauto/zippyusedauto/view/logout.php
+++
@@ -1,6 +0,0 @@
-<?php include '../zippyusedauto/view/header.php'; ?>
-
-    <h2>Thank you for signing out, <?php echo $_SESSION['userid']?></h2>
-    <h5><a href="?action=list_vehicles">Click here</a> to view our vehicle list</h5>
-
-<?php include '../zippyusedauto/view/footer.php'; ?>
